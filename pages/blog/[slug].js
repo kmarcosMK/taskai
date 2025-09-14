2108deade322c0a3f551478443a37ec1ed2bbbd8
kmarcosMK <kmarcos@wp.pl>
2025-09-14 15:34:55 +0200

    Add files via upload

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,1 +1,26 @@
-import { posts } from '../../lib/posts'; export default function Post({post}){return <div className='container section'>Post</div>} export async function getStaticPaths(){ const paths = posts.map(p=>({params:{slug:p.slug}})); return {paths,fallback:false}} export async function getStaticProps({params}){ const post = posts.find(p=>p.slug===params.slug)||null; return {props:{post}} }
+import { getPostBySlug, getSortedPostsMeta } from '../../lib/posts'
+
+export async function getStaticPaths(){
+  const posts = getSortedPostsMeta()
+  return {
+    paths: posts.map(p=>({ params: { slug: p.slug } })),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}){
+  const post = await getPostBySlug(params.slug)
+  return { props:{ post } }
+}
+
+export default function Post({post}){
+  return (
+    <div className="page">
+      <main className="container section">
+        <h1>{post.title}</h1>
+        <p style={{color:'#999'}}>{post.date}</p>
+        <article className="article" dangerouslySetInnerHTML={{__html: post.contentHtml}} />
+      </main>
+    </div>
+  )
+}
